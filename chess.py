b87ab9c4ea452e9eafa5c6b59b583b760799e083
laccces <en07djc@icloud.com>
2023-06-02 13:36:45 +0100

    Added pieces as letters

--- chess.py
+++ chess.py
@@ -1,6 +1,22 @@
-board = [['.' for _ in range(8)] for _ in range(8)]
+board = [['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
+         ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+         ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+         ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']]
+
 
 divider = "  -------------------------------"
+
+# 'K' and 'k' for King, 'Q' and 'q' for Queen, etc.
+# Uppercase letters for white pieces, lowercase for black
+unicode_pieces = {
+    'r': '\u265C', 'n': '\u265E', 'b': '\u265D', 'q': '\u265B', 'k': '\u265A', 'p': '\u265F',
+    'R': '\u2656', 'N': '\u2658', 'B': '\u2657', 'Q': '\u2655', 'K': '\u2654', 'P': '\u2659',
+    ' ': ' '
+}
 
 def print_board(board):
     print("Here's the board:")
